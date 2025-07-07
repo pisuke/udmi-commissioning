f291197723639807631a7f51194869cd97959d3d
Francesco Anselmo <fanselmo@google.com>
2025-07-07 12:05:04 +0100

    sheet2mangojson pyinstaller scripts

---
+++ pyinstaller-appbuilder-sheet2mangojson-rocky810.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+set -x
+ROOT_DIR=$(realpath $(dirname $0)/)
+
+TMP_DIR=$(mktemp -d)
+OUT_DIR=$ROOT_DIR/dist
+OUT_FILE=$OUT_DIR/sheet2mangojson-rocky810
+
+echo Building binary to 
+cat >$TMP_DIR/build.sh <<-EOF
+            set -x
+            rm -rf /tmp/sheet2mangojson
+            rm -rf /dist/sheet2mangojson
+            mkdir /build
+            cp -r /src/sheet2mangojson.py /build
+            cp -r /src/requirements.txt /build
+            cd /build
+            python3 -m pip install --upgrade pip
+            python3 -m pip install -r requirements.txt
+            pyinstaller --onefile --hidden-import=pandas sheet2mangojson.py
+            ls -la dist/*
+            mv dist/sheet2mangojson /tmp/sheet2mangojson
+EOF
+
+docker run --rm --volume $ROOT_DIR/:/src --volume $TMP_DIR:/tmp pyinstaller-builder-rocky-810:latest /bin/bash /tmp/build.sh
+mkdir -p $OUT_DIR
+mv $TMP_DIR/sheet2mangojson $OUT_FILE
+chmod 777 $OUT_FILE
